e4b41409ba2802d755aac6183ace2b42b47bbefb
Almoutasem Barbour <b.almoutasem@gmail.com>
2025-07-03 20:57:56 +0400

    Fix server entry point

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.static('public')); // serve static files
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Mailora is running 🎉');
+  res.send('Mailora is live 🎉');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
